3ebab2b8543bb4947babd003b6af24d6b9f4669b
Meghan <hello@nektro.net>
2021-01-06 23:02:24 -0800

    add a better example

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,25 @@ const std = @import("std");
 
 const tracy = @import("./lib.zig");
 
+var count: usize = 0;
+
 pub fn main() anyerror!void {
     const t = tracy.trace(@src());
     defer t.end();
+
     std.log.info("All your codebase are belong to us.", .{});
+
+    if (count >= 10) {
+        return;
+    }
+    count += 1;
+
+    var buf: [8]u8 = undefined;
+    try std.crypto.randomBytes(buf[0..]);
+    const seed = std.mem.readIntLittle(u64, buf[0..8]);
+    var r = std.rand.DefaultPrng.init(seed);
+    const w = r.random.int(u64);
+
+    std.time.sleep(1000_000_000 * (w % 5));
+    try main();
 }
